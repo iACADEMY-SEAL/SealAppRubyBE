3cb22478659da8039118f53acbbb69c98eeb8128
RemsonIO <joshua.remson@gmail.com>
2016-12-04 01:41:58 +0800

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'hello_seal', to: 'static#hello_html'
+  root to: 'static#hello_html'
 
 
   # V1 Nov. 2016
